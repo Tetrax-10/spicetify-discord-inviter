27e5bfd7155ada29a05be235a8179f02d9fdb1a7
Tetrax-10 <raghavanmarch2002@gmail.com>
2023-05-30 19:06:23 +0000

    Update dist to output generated at 164ad77

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-import tetraxDiscordInviter from "./tetraxDiscordInviter"
-
-export default async function initNord() {
-    while (!Spicetify.PopupModal.display) {
-        await new Promise((resolve) => setTimeout(resolve, 10))
-    }
-
-    tetraxDiscordInviter()
-}
